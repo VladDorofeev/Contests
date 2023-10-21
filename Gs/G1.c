48bb7cc6b5b9ace8b61a8bfcc10bc810533f58dc
Vlad_WIN <328777dv@bk.ru>
2023-10-21 14:12:48 +0300

    add spaces G1.c

--- Gs/G1.c
+++ Gs/G1.c
@@ -5,7 +5,7 @@ main(int argc, char **argv) {
     FILE *fp = fopen(argv[1], "r");
 
     int chr;
-    int cnt_str = 1;
+    int cnt_str = 1;    
     while ((chr = getc(fp)) != EOF) {
         if (cnt_str % 2 == 0) {
             printf("%c", chr);
