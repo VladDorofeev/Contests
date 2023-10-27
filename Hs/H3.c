5304655c87a8992e70d294eb52005378d0c44e43
VladDebian <deb>
2023-10-27 12:01:34 +0300

    Some rework H3.c and H4.c (fix data type to const)

--- Hs/H3.c
+++ Hs/H3.c
@@ -1,49 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
-enum { MAX_SWITCH_SIZE = 256};
+enum { MAX_SWITCH_SIZE = 256, MAX_STRING_SIZE = 82 };
 
-typedef void (*Str_Case) (char *);
+typedef void (*StrCase) (const char *);
 
-typedef struct{
-    char *str;
-    Str_Case func;
+typedef struct
+{
+    char const str[MAX_STRING_SIZE];
+    const StrCase func;
 } Pair;
 
-typedef const Pair Str_Switch[MAX_SWITCH_SIZE];
+typedef const Pair StrSwitch[MAX_SWITCH_SIZE];
 
 void
-run_str_switch(Str_Switch _switch, char *s) {
-    int i;
-    for (i = 0; strlen(_switch[i].str) != 0; ++i) {
-        if (strcmp(_switch[i].str, s) == 0) {
+run_str_switch(const StrSwitch *_switch, char *s) {
+    size_t i;
+    for (i = 0; strlen((*_switch)[i].str) != 0; ++i) {
+        if (strcmp((*_switch)[i].str, s) == 0) {
             //Found s in our cases
-            if (_switch[i].func != NULL) {
-                _switch[i].func(s);
+            if ((*_switch)[i].func != NULL) {
+                (*_switch)[i].func(s);
             }
             return;
         }
     }
     //Default
-    _switch[i].func(s);
+    (*_switch)[i].func(s);
 }
 
 void
-case_add (char *s) {
+case_add (const char *s) {
     printf("ADD\n");
 }
 
 void
-case_sub (char *s) {    
+case_sub (const char *s) {    
     printf("SUB\n");
 }
 
 void
-case_default (char *s) {
+case_default (const char *s) {
     char *end;
     long num = strtol(s, &end, 10);
-    if ((*end ==0) && ((int)num == num) && (strlen(s) != 0)) {
+    if ((*end == 0) && (num <= INT_MAX) && (strlen(s) != 0)) {
         printf("NUMBER\n");
     } else {
         printf("UNKNOWN\n");
@@ -54,11 +56,14 @@ case_default (char *s) {
 
 int
 main (int argc, char **argv) {
+    //argc = 3;
+    //argv[1] = "add";
+    //argv[2] = "sub";
+
     argc--;
     argv++;
 
-    
-    Str_Switch _switch = {
+    const StrSwitch _switch = {
         {"add", case_add}, 
         {"sub", case_sub},
         {"", case_default},
@@ -66,7 +71,7 @@ main (int argc, char **argv) {
 
 
     for (int i = 0; i < argc; ++i) {
-        run_str_switch(_switch, argv[i]);
+        run_str_switch(&_switch, argv[i]);
     }
     
     return 0;
