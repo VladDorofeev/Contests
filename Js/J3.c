f34cb7647b346b91be664574d89b7b5f544a5d1e
VladDebian <deb>
2023-11-03 11:47:35 +0300

    Rework J3.c (return not negative num)

--- Js/J3.c
+++ Js/J3.c
@@ -11,14 +11,14 @@ main (int argc, char **argv) {
     argv[argc - 1] = NULL;
     if (fork() == 0) {
         execvp(argv[1], &argv[1]);
-        return -1;
+        return 1;
     }
     wait(&status);
 
     if ((WIFEXITED(status)) && (WEXITSTATUS(status) == 0)) {
         if (fork() == 0) {
             execlp(pr_2, pr_2, NULL);
-            return -1;
+            return 1;
         }
         wait(&status);
     }
