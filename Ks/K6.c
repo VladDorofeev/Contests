062838afb6d071208e58a43bc857519ac3c76cd9
VladDebian <deb>
2023-11-11 15:51:47 +0300

    K6.c done (work with functions)

--- Ks/K6.c
+++ Ks/K6.c
@@ -5,61 +5,67 @@
 #include <wait.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 
+void 
+special_write (int fd_read, int fd_write, char *sym) {
+    off_t writer_pos = lseek(fd_write, 0, SEEK_CUR);
+    if (writer_pos == 32) {
+        writer_pos = lseek(fd_write, 0, SEEK_SET);
+    }
+    while ((writer_pos == lseek(fd_read, 0, SEEK_CUR) - 1) || 
+            (writer_pos - lseek(fd_read, 0, SEEK_CUR) == 31)) {
+        usleep(500);
+    }
+    write(fd_write, sym, 1);
+}
+
+
+int
+special_read(int fd_read, int fd_write, char *sym) {
+    off_t reader_pos = lseek(fd_read, 0, SEEK_CUR);
+    if (reader_pos == 32) {
+        reader_pos = lseek(fd_read, 0, SEEK_SET);
+    }
+    while (reader_pos == lseek(fd_write, 0, SEEK_CUR)) {
+        if (waitpid(-1, 0, WNOHANG) != 0) {
+            return -1;
+        }
+        usleep(500);
+    }
+    read(fd_read, sym, 1);
+    return 0;
+}
 
 int 
 main (void) {
-    char temp_name[7] = "XXXXXX";
+    char temp_name[] = "tempXXXXXX";
     int temp_fd = mkstemp(temp_name);
     close(temp_fd);
     int fd_write = open(temp_name, O_WRONLY);
     int fd_read = open(temp_name, O_RDONLY);
     unlink(temp_name);
 
+
     if (fork() == 0) {
         //Writer
         char input;
-        while (read(0, &input, 1) != 0) {
-            off_t pos_wr = lseek(fd_write, 0, SEEK_CUR);
-            
-            //Stop one char before fd_read
-            while (lseek(fd_read, 0, SEEK_CUR) - pos_wr == 1) {
-                usleep(1000);
-            }
-            while (pos_wr - lseek(fd_read, 0, SEEK_CUR) == 32) {
-                usleep(1000);
-            }
-
-            if (pos_wr == 32) {
-                lseek(fd_write, 0, SEEK_SET);
-            }
-
-            write(fd_write, &input, 1);
+        while (read(STDIN_FILENO, &input, 1) != 0) {
+            special_write(fd_read, fd_write, &input);
         }
+        close(fd_read);
         close(fd_write);
         return 0;
-    } else {
-        //Reader
-        char output;
-        for (;;) {
-            off_t pos_rd = lseek(fd_read, 0, SEEK_CUR);
+    }
+    //Reader
+    char output;
 
-            while (lseek(fd_write, 0, SEEK_CUR) == pos_rd) {
-                if (waitpid(-1, 0, WNOHANG) != 0) {
-                    close(fd_read);
-                    return 0;
-                }
-                usleep(500);
-            }
-
-            if (pos_rd == 32) {
-                lseek(fd_read, 0, SEEK_SET);
-                pos_rd = 0;
-            }
-
-            read(fd_read, &output, 1);
-            write(1, &output, 1);
+    while (1) {
+        if (special_read(fd_read, fd_write, &output) == -1) {
+            close(fd_read);
+            close(fd_write);
+            return 0;
         }
+        write(STDOUT_FILENO, &output, 1);
+        
     }
 }
