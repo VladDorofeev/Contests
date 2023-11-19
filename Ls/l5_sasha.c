c098f9e5447967703e8f50cc023bf9384a8deaad
kirichek_sasha <kirichek_sasha@hill.cs.msu.ru>
2023-11-19 21:57:39 +0300

    rework l5 by sasha

--- Ls/l5_sasha.c
+++ Ls/l5_sasha.c
@@ -5,75 +5,74 @@
 #include <sys/types.h>
 
 void
-process_a(int d_pipe1, int d_pipe2){
-    int num_arr[2];
-    int rez[2];
-    while(read(d_pipe1, &num_arr[0], 8) == 8) {
-        rez[0] = num_arr[0] + num_arr[1];
-        rez[1] = num_arr[0] - num_arr[1];
-        write(d_pipe2, &rez[0], 8);
+do_process_a(void) {
+    int nums[2];
+    int calculations[2];
+    while(read(0, &nums[0], 2 * sizeof(int)) == 2 * sizeof(int)) {
+        calculations[0] = nums[0] + nums[1];
+        calculations[1] = nums[0] - nums[1];
+        write(1, &calculations[0], 2 * sizeof(int));
     }
 }
 
 void
-do_son_process(int d_pipe1, int d_pipe2, int d_pipe3, int nums[2]) {
-    int rez[2];
-    write(d_pipe1, nums, 8);
-    while(read(d_pipe2, &rez[0], 8) == 8) {
-        if ((nums[1] + nums[0] == rez[0]) && (nums[0] - nums[1] == rez[1])) {
-            break;
-        }
-        else {
-            write(d_pipe3, &rez[0], 8);
-        }
-    }
-    printf("%d %d %d %d\n", nums[0], nums[1], rez[0], rez[1]);
+do_son_process(int fd,int nums[]) {
+    int caclilations[2];
+    write(fd, &nums[0], 2 * sizeof(int));
+    read(0, &caclilations[0], 2 * sizeof(int));
+    printf("%d %d %d %d\n", nums[0], nums[1], caclilations[0], caclilations[1]);
 }
 
 int
-main(void) { 
+main(void) {
     setbuf(stdin, 0);
-    int fd[2];
-    int fd_sec[2];
-    pipe(fd);
-    pipe(fd_sec);
+    int fd_nums_to_a[2];
+    int fd_calc_from_a[2];
+    pipe(fd_nums_to_a);
+    pipe(fd_calc_from_a);
+    //process A
     if (fork() == 0) {
-        close(fd_sec[0]);
-        close(fd[1]);
-        process_a(fd[0], fd_sec[1]);
-        close(fd[0]);
-        close(fd_sec[1]);
+        close(fd_nums_to_a[1]);
+        close(fd_calc_from_a[0]);
+        dup2(fd_nums_to_a[0], 0);
+        dup2(fd_calc_from_a[1], 1);
+        close(fd_nums_to_a[0]);
+        close(fd_calc_from_a[1]);
+        do_process_a();
         exit(0);
     }
-    int two_num[2];
+    //other sons
+    int fd_ban_all_son[2];
+    int two_nums[2];
     pid_t pid;
-    while(scanf("%d %d", &two_num[0], &two_num[1]) == 2) {
-        if ((pid =fork()) == 0) {
-            close(fd[0]);
-            do_son_process(fd[1], fd_sec[0], fd_sec[1], two_num);
-            close(fd_sec[1]);
-            close(fd[1]);
-            close(fd_sec[0]);
+    pipe(fd_ban_all_son);
+    char open_byte = 1;
+    write(fd_ban_all_son[1], &open_byte, sizeof(char));
+    close(fd_nums_to_a[0]);
+    close(fd_calc_from_a[1]);
+    //sons
+    while(scanf("%d%d", &two_nums[0], &two_nums[1]) == 2) {
+        while((pid = fork()) == -1) {
+            wait(NULL);
+        }
+        if (pid == 0) {
+            dup2(fd_calc_from_a[0], 0);
+            close(fd_calc_from_a[0]);
+            //if process can read then it can start and other sons blocks 
+            read(fd_ban_all_son[0], &open_byte, sizeof(char));
+            do_son_process(fd_nums_to_a[1] ,two_nums);
+            //process done then one of the others son processes can start
+            write(fd_ban_all_son[1], &open_byte, sizeof(char));
+            close(fd_nums_to_a[1]);
+            close(fd_ban_all_son[0]);
+            close(fd_ban_all_son[1]);
             exit(0);
-        } else if (pid == -1) {
-            while(pid == -1) {
-                wait(NULL);
-                pid = fork();
-            }
-            if (pid == 0) {
-                close(fd[0]);
-                do_son_process(fd[1], fd_sec[0], fd_sec[1], two_num);
-                close(fd_sec[1]);
-                close(fd[1]);
-                close(fd_sec[0]);
-                exit(0);
-            }
         }
     }
-    close(fd[0]);
-    close(fd_sec[1]);
-    close(fd[1]);
-    close(fd_sec[0]);
+    close(fd_nums_to_a[1]);
+    close(fd_calc_from_a[0]);
+    close(fd_ban_all_son[0]);
+    close(fd_ban_all_son[1]);
     while(wait(NULL) != -1) {
     }
     return 0;
