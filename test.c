48833312f4c87c8b1fa326575e1dd2f914218002
VladDebian <deb>
2023-11-20 22:19:43 +0300

    test.c testing retranslate signals

--- test.c
+++ test.c
@@ -1,60 +1,35 @@
+#include <unistd.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+pid_t pid = 0;
+
+
+void
+sig_hndl(int sig) {
+    printf("zxc\n");
+}
+
+
+void
+sig_hndl2(int sig) {
+    if (pid != 0) {
+        kill(pid, SIGINT);
+    }
+}
 
 int
-main(void)
-{
-    int fd[2];
-    int fd_2[2];
-    pipe(fd);
-    pipe(fd_2);
-
-    size_t int_s = sizeof(int);
-
-    if (fork() == 0) {
-        //pipe_1[read]
-        //pipe_2[write]
-        dup2(fd[0], STDIN_FILENO);
-        dup2(fd_2[1], STDOUT_FILENO);
-
-        close(fd[0]);
-        close(fd[1]);
-        close(fd_2[0]);
-        close(fd_2[1]);
-
-        int num;
-        while(read(STDIN_FILENO, &num, int_s) == int_s) {
-            write(STDOUT_FILENO, &num, int_s);
-        }
-        return 0;
+main (void) {
+    if ((pid = fork()) == 0) {
+        signal(SIGINT, sig_hndl);
+        while(1);
     }
-    
-    int num = 1;
-
-    //Unused pipe
-    close(fd[0]);
-    close(fd_2[1]);
-
-    //Write to process A
-    write(fd[1], &num, int_s);
-    close(fd[1]);
-
-
-    sleep(1);
-
-
-    //Read from process A
-    read(fd_2[0], &num, int_s);
-    printf("main num = %d\n", num);
-    close(fd_2[0]);
-
-
-    while(wait(NULL) != -1);
+    signal(SIGINT, sig_hndl2);
+    wait(NULL);
     return 0;
 }
