2992fa817c0268a771ae017ca41d4da9cf904a4a
VladDebian <deb>
2024-02-15 19:44:08 +0300

    A3 rename include files

--- Spring/As/A3/solution/narray.c
+++ Spring/As/A3/solution/narray.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "Header.h"
+#include "narray.h"
 
 int *nums = 0;
 int c = 0;
--- Spring/As/A3/solution/solution.c
+++ Spring/As/A3/solution/solution.c
@@ -1,4 +1,4 @@
-#include "Header.h"
+#include "narray.h"
 
 int
 main(void)
