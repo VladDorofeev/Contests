0fc8d8601f91780f43b6b675d64e4f9cae105e37
VladDebian <deb>
2024-02-23 16:46:23 +0300

    B2 OK, rework sort

--- Spring/Bs/B2.cpp
+++ Spring/Bs/B2.cpp
@@ -4,19 +4,31 @@
 
 enum { START_SZ = 3 };
 
+static int compare_ints(const void* a, const void* b)
+{
+    int arg1 = *static_cast<const int*>(a);
+    int arg2 = *static_cast<const int*>(b);
+ 
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+
 class Seq 
 {
-    int *arr;
-    int cap;
-    int len;
 public:
     Seq();
     ~Seq();
     void add_num(int num);
     void sort();
 
-    int get_len();
-    int get_num(int i);
+    int get_len () const;
+    int get_num (int i) const;
+
+private:
+    int *arr;
+    int cap;
+    int len;
 };
 
 int
@@ -65,16 +77,19 @@ Seq::add_num(int num) {
 
 void
 Seq::sort() {
-    std::sort(this->arr, this->arr + this->len);
+    if (this->arr != NULL) {
+        qsort(this->arr, this->len, sizeof *this->arr, compare_ints);
+    }
 }
 Seq::~Seq() {
     delete[] this->arr;
 }
 
-int Seq::get_len() {
+int Seq::get_len() const {
     return this->len;
 }
 
-int Seq::get_num(int i) {
+int Seq::get_num(int i) const {
     return this->arr[i];
 }
+
