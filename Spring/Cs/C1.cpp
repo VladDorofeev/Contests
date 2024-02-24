0f57ea05d20c6ce19c6c186647bf13df0b2e20f3
VladDebian <deb>
2024-02-24 09:44:35 +0300

    C1 OK (added copy c-tor, operator =)

--- Spring/Cs/C1.cpp
+++ Spring/Cs/C1.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
-
 class Seq
 {
 public:
     Seq(int _sz);
+    Seq(Seq &sq);
+    Seq& operator =(const Seq &sq);
     ~Seq();
-    int &item(int pos);
-    friend void print_seq(Seq &sq);
+
+    int &item(int pos) const;
+    friend void print_seq(const Seq &sq);
 private:
     int *arr;
     int sz;
@@ -24,8 +26,26 @@ Seq::~Seq()
     delete[] arr;
 }
 
+Seq::Seq(Seq &sq) {
+    arr = new int[sq.sz];
+    for (int i = 0; i < sq.sz; i++) {
+        arr[i] = sq.arr[i];
+    }
+    sz = sq.sz;
+}
+
+Seq& 
+Seq::operator =(const Seq &sq) {
+    arr = new int[sq.sz];
+    for (int i = 0; i < sq.sz; i++) {
+        arr[i] = sq.arr[i];
+    }
+    sz = sq.sz;
+    return *this;
+}
+
 int &
-Seq::item(int pos) {
+Seq::item(int pos) const {
     if (pos >= sz) {
         std::cout << "Out of bound" << std::endl;
         delete[] this->arr;
@@ -35,7 +55,7 @@ Seq::item(int pos) {
 }
 
 void
-print_seq(Seq &sq) {
+print_seq(const Seq &sq) {
     for (int i = 0; i < sq.sz - 1; i++) {
         std::cout << sq.arr[i] << ' ';
     }
