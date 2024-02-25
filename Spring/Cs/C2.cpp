48b7f3137e39fe1e7f2978e22384cbc323dc8192
VladDebian <deb>
2024-02-25 22:42:56 +0300

    C2 developing

---
+++ Spring/Cs/C2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+
+class UniquePtr
+{
+public:
+    UniquePtr();
+
+    UniquePtr(UniquePtr &uptr);
+    UniquePtr(UniquePtr &&uptr);
+
+    const UniquePtr& operator= (UniquePtr &uptr);
+    const UniquePtr& operator= (UniquePtr &&uptr);
+
+    ~UniquePtr();
+
+    char get(int pos) const;
+    friend UniquePtr make(int sz);
+private:
+    char *ptr;
+    int sz;
+    UniquePtr(int _sz);
+};
+
+using namespace std;
+
+UniquePtr::UniquePtr() {
+    cout << "def ctor" << endl;
+    this->ptr = nullptr;
+    this->sz = 0;
+}
+UniquePtr::UniquePtr(UniquePtr &uptr) {
+    cout << "copy ctor" << endl;
+    ptr = uptr.ptr;
+    sz = uptr.sz;
+    uptr.ptr = nullptr;
+}
+UniquePtr::UniquePtr(UniquePtr &&uptr) {
+    cout << "move ctor" << endl;
+    ptr = uptr.ptr;
+    sz = uptr.sz;
+    uptr.ptr = nullptr;
+}
+UniquePtr::UniquePtr(int _sz) {
+    cout << "params ctor" << endl;
+    ptr = new char[_sz];
+    sz = _sz;
+}
+
+const UniquePtr& 
+UniquePtr::operator= (UniquePtr &uptr) {
+    cout << "oper = &" << endl;
+    ptr = uptr.ptr;
+    sz = uptr.sz;
+    uptr.ptr = nullptr;
+    return *this;
+}
+const UniquePtr& 
+UniquePtr::operator= (UniquePtr &&uptr) {
+    cout << "oper = &&" << endl;
+    ptr = uptr.ptr;
+    sz = uptr.sz;
+    uptr.ptr = nullptr;
+    return *this;
+}
+
+UniquePtr::~UniquePtr() {
+    cout << "destructor" << endl;    
+    delete[] ptr;
+}
+
+char 
+UniquePtr::get(int pos) const {
+    cout << "get" << endl;
+    return ptr[pos];
+}
+
+UniquePtr
+make(int _sz) {
+    cout << "start make" << endl;
+
+    cout << "end make" << endl;
+    return UniquePtr(_sz);
+}
+
+int
+main() {
+    UniquePtr uptr = make(10000);
+    cout << endl;
+
+    UniquePtr uptr2;
+    uptr2 = uptr;
+    cout << endl;
+
+    UniquePtr uptr3;
+    uptr3 = make(10000);
+    cout << endl;
+
+    const UniquePtr uptr4 = uptr3;
+
+
+    return 0;
+}
