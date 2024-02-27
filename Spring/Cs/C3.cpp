235b1ebfca4a07527502d8e6e1a1ad1434d96b68
VladDebian <deb>
2024-02-27 17:53:56 +0300

    C3 in working...

--- Spring/Cs/C3.cpp
+++ Spring/Cs/C3.cpp
@@ -1,107 +1,84 @@
 #include <iostream>
-class DebugPrinter;
+
+
+class DebugPrinter 
+{
+public: 
+    static DebugPrinter *get();
+    ~DebugPrinter();
+
+    DebugPrinter &out(int); 
+    DebugPrinter &out(char) ; 
+
+    DebugPrinter( DebugPrinter&) = delete;
+    DebugPrinter operator=(DebugPrinter &) = delete;
+private:
+    static  DebugPrinter *ptr;
+    DebugPrinter();
+};
+
+DebugPrinter *DebugPrinter::ptr = nullptr;
+
+DebugPrinter*
+DebugPrinter::get() {
+    if (ptr == nullptr) {
+        static DebugPrinter dbg;
+        ptr = &dbg;
+    }
+    return ptr;
+}
+
+DebugPrinter::~DebugPrinter() {
+    std::cout << "Debugger is destroyed." << std::endl;
+}
+
+DebugPrinter::DebugPrinter() {
+    std::cout << "Debugger is created." << std::endl;
+}
+
+DebugPrinter &
+DebugPrinter::out(int n)  {
+    std::cout << n;
+    return *this;
+} 
+
+DebugPrinter &
+DebugPrinter::out(char c)  {
+    std::cout << c;
+    return *this;
+}
 
 class IntCharPair
 {
 public:
     IntCharPair(int, char);
     // not more than one new declaration is allowed here
-    friend class DebugPrinter;
+    friend DebugPrinter& out(DebugPrinter&, IntCharPair&);
 private:
     int n;
     char c;
 };
-
-class DebugPrinter 
-{
-public: 
-    ~DebugPrinter();
-
-    static DebugPrinter &get();
-
-    DebugPrinter &out(int);
-    DebugPrinter &out(char);
-
-    void set(IntCharPair &pair);
-
-    friend DebugPrinter& out(DebugPrinter &dbg, IntCharPair &pair);
-
-private:
-    DebugPrinter(const DebugPrinter &dbg) = delete;
-    DebugPrinter& operator=(DebugPrinter &) = delete;
-
-    DebugPrinter();
-    
-    int n;
-    char c;
-
-    static int cnt;
-};
-
-int DebugPrinter::cnt = 0;
-
-DebugPrinter::DebugPrinter() {
-    this->cnt++;
-    this->n = 0;
-    this->c = 0;
-    std::cout << "Debugger is created." << std::endl;
-}
-
-DebugPrinter::~DebugPrinter() {
-    if (this->cnt == 0) {
-        exit(0);
-    }
-    std::cout << "Debugger is destroyed." << std::endl;
-    this->cnt--;
-}
-
-DebugPrinter&
-DebugPrinter::get() {
-    static DebugPrinter dbg = DebugPrinter();
-    return dbg;
-}
-
-
-DebugPrinter&
-DebugPrinter::out(int num) {
-    std::cout << num;
-    return *this;
-}
-
-DebugPrinter&
-DebugPrinter::out(char sym) {
-    std::cout << sym;
-    return *this;
-}
-
-
-DebugPrinter&
-out(DebugPrinter &dbg, IntCharPair &pair) {
-    dbg.set(pair);
-    dbg.out(dbg.n);
-    dbg.out(dbg.c);
-    return dbg;
-}
 
 IntCharPair::IntCharPair(int _n, char _c) {
     n = _n;
     c = _c;
 }
 
-void
-DebugPrinter::set(IntCharPair &pair) {
-    n = pair.n;
-    c = pair.c;
-} 
-
+DebugPrinter&
+out(DebugPrinter &dbg, IntCharPair &pair) {
+    dbg.out(pair.c);
+    dbg.out(pair.n);
+    return dbg;
+}
 
 #ifdef _main
 
 int
 main() {
-    IntCharPair pair(228, 't');
-    out(DebugPrinter::get().out('q'), pair).out(123).out('\n');
+    DebugPrinter *dbg = DebugPrinter::get();
+    IntCharPair pair(123, 'q');
 
+    out(dbg->out('>'), pair).out('<');
     return 0;
 }
 
