b42be62a855c12689fe34560b5348e602d36acf9
VladDebian <deb>
2024-03-01 21:39:57 +0300

    Add D5 by Vlad

---
+++ Spring/Ds/D5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int
+main() {
+    std::cout << "1 1" << std::endl; 
+    std::cout << "2 1" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "1 2" << std::endl; 
+    std::cout << "-1" << std::endl; 
+    std::cout << "3 3" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "11 4" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "7 1 1" << std::endl; 
+    std::cout << "8 1 1" << std::endl; 
+    std::cout << "0" << std::endl; 
+    std::cout << "8 3 1" << std::endl; 
+    std::cout << "-1" << std::endl; 
+    return 0;
+}
