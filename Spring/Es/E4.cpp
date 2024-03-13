735e47885d0966c2711d910e664257bfec8d5b74
VladDebian <deb>
2024-03-13 22:48:08 +0300

    E4 OK

---
+++ Spring/Es/E4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int
+main() {
+    std::cout << "D()" << std::endl;
+    std::cout << "A()" << std::endl;
+    std::cout << "B()" << std::endl;
+    std::cout << "A(const A &)" << std::endl;
+    std::cout << "E()" << std::endl;
+    std::cout << "A()" << std::endl;
+    std::cout << "B()" << std::endl;
+    std::cout << "C()" << std::endl;
+    std::cout << "~C()" << std::endl;
+    std::cout << "~B()" << std::endl;
+    std::cout << "~A()" << std::endl;
+    std::cout << "~E()" << std::endl;
+    std::cout << "~A()" << std::endl;
+    std::cout << "~B()" << std::endl;
+    std::cout << "~A()" << std::endl;
+    std::cout << "~D()" << std::endl;
+    return 0;
+}
