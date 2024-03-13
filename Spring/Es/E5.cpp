d82169c1914784ffc3dfe9663b4ccf89be956767
VladDebian <deb>
2024-03-14 00:33:03 +0300

    E5 OK

---
+++ Spring/Es/E5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int
+main() {
+    std::cout << "1 0" << std::endl;
+    std::cout << "2 B" << std::endl;
+    std::cout << "3 B" << std::endl;
+    std::cout << "4 0" << std::endl;
+    std::cout << "5 B" << std::endl;
+    std::cout << "6 0" << std::endl;
+    std::cout << "7 0" << std::endl;
+    std::cout << "8 0" << std::endl;
+    std::cout << "9 B" << std::endl;
+    std::cout << "10 C" << std::endl;
+    std::cout << "11 0" << std::endl;
+    return 0;
+}
