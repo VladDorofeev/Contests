701d067667b83183a24d6fceeb37a4f3acf6a96b
Vladislav <328777dv@bk.ru>
2024-03-22 18:29:02 +0300

    F5 OK

---
+++ Spring/Fs/F5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "bear" << std::endl;
+    std::cout << "diameter" << std::endl;
+    std::cout << "result = (76;6)" << std::endl;
+    std::cout << "central" << std::endl;
+    std::cout << "abba" << std::endl;
+    return 0;
+}
