4d757e4318d03c3e219afefe6289b5083f20c90f
Киричек Александр <kiricekaleksandr@MacBook-Pro-Kiricek.local>
2024-04-22 15:18:53 +0300

    add j5 and tests for j5 by sasha

--- Spring/Js/j5_sasha.cpp
+++ Spring/Js/j5_sasha.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
-#include <functional>
+
+
 using Rule = std::pair<std::string, std::string>;
 using Grammar = std::set<Rule>;
 using Symbols = std::set<char>;
 
-bool is_terminal(char sym) {
-    if (!std::isupper(sym)) {
-        return true;
+
+std::istream& operator>>(std::istream& in, Grammar& g) {
+    std::string str;
+    while(std::getline(in, str)) {
+        g.insert({str.substr(0, str.find(':')), str.substr(str.find(':') + 1)});
     }
-    return false;
+    return in;
 }
 
-void make_new_string(Rule const& rule, Symbols& empty_sym, long unsigned int index, std::string str, std::vector<Rule>& comb) {
+
+std::ostream& operator<<(std::ostream& out, Grammar& g) {
+    std::for_each(g.begin(), g.end(), [&out](Rule const& rule){
+        out << rule.first << ':' << rule.second << std::endl;
+    });
+    return out;
+}
+
+
+
+void make_new_string(Rule const& rule, Symbols& empty_sym, long unsigned int index, std::string str, Grammar& comb) {
     if (index == rule.second.size()) {
-        if (str != "") {
-            comb.push_back({rule.first, str});
-        }
-        if (str == "" && rule.first == "S") {
-            comb.push_back({"T", ""});
-            comb.push_back({"T", "S"});
+        if (!str.empty()) {
+            comb.insert({rule.first, str});
         }
         return;
     }
@@ -30,22 +39,19 @@ void make_new_string(Rule const& rule, Symbols& empty_sym, long unsigned int ind
     make_new_string(rule, empty_sym, index + 1, str + rule.second[index], comb);
 }
 
-void make_new_combinations(Rule const& rule, Symbols& empty_sym, std::vector<Rule>& new_comb) {
-    make_new_string(rule, empty_sym, 0, "", new_comb);
-}
 
-
-bool contain_non_good_non_terminal(std::string str, Symbols& non_terminals) {
+bool contain_non_good_non_terminal(std::string const& str, Symbols& non_terminals) {
     if (std::any_of(str.begin(), str.end(), [&non_terminals](char sym){
         if (!isupper(sym)) {
             return false;
         }
-        return std::find(non_terminals.begin(), non_terminals.end(), sym) == non_terminals.end() ? true : false;
+        return std::find(non_terminals.begin(), non_terminals.end(), sym) == non_terminals.end();
     })) {
         return true;
     }
     return false;
 }
+
 
 void add_rule(Grammar& g, std::string& s) {
     std::string::size_type delimiter_pos = s.find(':');
@@ -56,35 +62,30 @@ void add_rule(Grammar& g, std::string& s) {
 void kill_useless_non_terminals(Grammar& g) {
     Symbols good_non_terminals;
     std::for_each(g.begin(), g.end(), [&good_non_terminals](Rule rule){
-        if (std::all_of(rule.second.begin(), rule.second.end(), is_terminal)) {
+        if (std::all_of(rule.second.begin(), rule.second.end(), [](char sym){
+            return !std::isupper(sym);
+        })) {
             good_non_terminals.insert(rule.first[0]);
         }
     });
     Symbols::size_type prev_size = good_non_terminals.size();
-    while(true) {
+    do {
+        prev_size = good_non_terminals.size();
         std::for_each(g.begin(), g.end(), [&good_non_terminals](Rule rule){
             if (std::all_of(rule.second.begin(), rule.second.end(), [&good_non_terminals](char& sym){
                 if (!std::isupper(sym)) {
                     return true;
                 }
-                return std::find(good_non_terminals.begin(), good_non_terminals.end(), sym) == good_non_terminals.end() ? false : true;
+                return std::find(good_non_terminals.begin(), good_non_terminals.end(), sym) != good_non_terminals.end();
             })) {
                 good_non_terminals.insert(rule.first[0]);
             }
         });
-        if (prev_size == good_non_terminals.size()) {
-            break;
-        }
-        prev_size = good_non_terminals.size();
-    }
-    for(Grammar::iterator it = g.begin(); it != g.end();) {
-        if (contain_non_good_non_terminal((*it).first, good_non_terminals)
-            || (contain_non_good_non_terminal((*it).second, good_non_terminals))) {
-            it = g.erase(it);
-        } else {
-            ++it;
-        }
-    }
+    } while(prev_size != good_non_terminals.size());
+    std::erase_if(g, [&good_non_terminals](Rule const& rule){
+        return contain_non_good_non_terminal(rule.first, good_non_terminals) 
+            || contain_non_good_non_terminal(rule.second, good_non_terminals);
+    });
 }
 
 
@@ -97,7 +98,8 @@ void kill_impossible_sym(Grammar& g) {
         good_non_terminals.insert('S');
     }
     Symbols::size_type prev_size = good_non_terminals.size();
-    while(true) {
+    do {
+        prev_size = good_non_terminals.size();
         std::for_each(g.begin(), g.end(), [&good_non_terminals](Rule rule){
             if (std::find(good_non_terminals.begin(), good_non_terminals.end(), rule.first[0]) != good_non_terminals.end()) {
                 std::for_each(rule.second.begin(), rule.second.end(), [&good_non_terminals](char sym){
@@ -107,80 +109,62 @@ void kill_impossible_sym(Grammar& g) {
                 });
             }
         });
-        if (prev_size == good_non_terminals.size()) {
-            break;
-        }
-        prev_size = good_non_terminals.size();
-    }
-    for(Grammar::iterator it = g.begin(); it != g.end();) {
-        if (contain_non_good_non_terminal((*it).first, good_non_terminals)
-            || (contain_non_good_non_terminal((*it).second, good_non_terminals))) {
-            it = g.erase(it);
-        } else {
-            ++it;
-        }
-    }
+    } while(prev_size != good_non_terminals.size());
+    std::erase_if(g, [&good_non_terminals](Rule const& rule){
+        return contain_non_good_non_terminal(rule.first, good_non_terminals) 
+            || contain_non_good_non_terminal(rule.second, good_non_terminals);
+    });
 }
 
 
 void kill_eps_rules(Grammar& g) {
     Symbols empty_non_terminals;
-    for(Grammar::iterator it = g.begin(); it != g.end();) {
-        if ((*it).second.empty()) {
-            empty_non_terminals.insert(it->first[0]);
-            it = g.erase(it);
-        } else {
-            ++it;
+    std::erase_if(g, [&empty_non_terminals](Rule const& rule){
+        if (rule.second.empty()) {
+            empty_non_terminals.insert(rule.first[0]);
+            return true;
         }
-    }
+        return false;
+    });
+    Symbols::size_type prev_size = empty_non_terminals.size();
+    do {
+        prev_size = empty_non_terminals.size();
+        std::for_each(g.begin(), g.end(), [&empty_non_terminals](Rule const& rule) {
+            if (std::all_of(rule.second.begin(), rule.second.end(), [&empty_non_terminals](char sym){
+                return std::find(empty_non_terminals.begin(), empty_non_terminals.end(), sym) != empty_non_terminals.end();
+            })){
+                empty_non_terminals.insert(rule.first[0]);
+            }
+        });
+    } while(prev_size != empty_non_terminals.size());
+    Grammar additional_rules;
+    std::for_each(g.begin(), g.end(), [&additional_rules, &empty_non_terminals](Rule const& rule){
+        make_new_string(rule, empty_non_terminals, 0, "", additional_rules);
+    });
     if (std::find(empty_non_terminals.begin(), empty_non_terminals.end(), 'S') != empty_non_terminals.end()) {
         g.insert({"T", ""});
         g.insert({"T", "S"});
     }
-    std::vector<Rule> new_rules;
-    for(Grammar::iterator it = g.begin(); it != g.end(); ++it) {
-        make_new_combinations(*it, empty_non_terminals, new_rules);
-    }
-    for(long unsigned int i = 0; i < new_rules.size(); ++i) { 
-        g.insert(new_rules[i]);
-    }
-    for(Grammar::iterator it = g.begin(); it != g.end();) {
-        if ((*it).first == (*it).second) {
-            it = g.erase(it);
-        } else {
-            ++it;
-        }
-    }
+    std::for_each(additional_rules.begin(), additional_rules.end(), [&g](Rule const& rule) {
+        g.insert(rule);
+    });
+    std::erase_if(g, [&empty_non_terminals](Rule const& rule){
+        return rule.first == rule.second;
+    });
 }
 
 
 void make_good_grammar(Grammar& g) {
-    kill_useless_non_terminals(g);
-    kill_impossible_sym(g);
     kill_eps_rules(g);
     kill_useless_non_terminals(g);
     kill_impossible_sym(g);
 }
 
 
-// S→Ac
-// A→SD
-// D→aD
-// A→a
-
-// S→AB|CD
-// A→EF
-// G→AD
-// C→c
 int
 main() {
     Grammar grammar;    
-    std::string str;
-    while (std::getline(std::cin, str)) {
-        add_rule(grammar, str);
-    }
+    std::cin >> grammar;
     make_good_grammar(grammar);
-    for(Grammar::iterator it = grammar.begin(); it != grammar.end(); ++it) {
-        std::cout << (*it).first << ":" << (*it).second << std::endl;
-    }
+    std::cout << grammar;
 }
