debd26c7613248812bc192afaaa16ad157edd691
Vladislav <328777dv@bk.ru>
2024-04-25 11:54:45 +0300

    J5 OK (refactoring, remove class Grammar)

--- Spring/Js/J5.cpp
+++ Spring/Js/J5.cpp
@@ -6,66 +6,31 @@
 
 namespace solution
 {
-typedef std::pair<std::string, std::string> Rule_type; 
-typedef std::set<Rule_type> Grammar_type;
+typedef std::pair<std::string, std::string> Rule; 
+typedef std::set<Rule> Rules;
 
-class Grammar
-{
-public:
-    Grammar(std::istream &);
-    Grammar(const Grammar &) = delete;
-    Grammar& operator=(const Grammar &) = delete;
-    ~Grammar() = default;
-
-
-    void cast();
-    void print() const;
-private:
-    //Useful methods
-    std::string get_str_from_char(char c);
-
-    //Step 1
-    std::pair<std::set<std::string>, Grammar_type> get_bad_non_term();
-    
-    //Step 2 (and 3 at the same moment)
-    void break_rules(std::set<std::string> &bad_non_term, Grammar_type bad_rules);
-    void proccess_bad_rule(const Rule_type &, const std::set<std::string> &, Grammar_type &);
-
-    //Remove X->X
-    void delete_simple_rules();
-
-    //Remove barren syms
-    void barren();
-    
-    //Remove unattainable syms
-    void unattainable();
-
-    //Fields
-    Grammar_type items;
-};
-
-Grammar::Grammar(std::istream &in) 
+void grammar_create(Rules &grammar, std::istream &in) 
 {
     std::string s;
     while (std::getline(in, s)) {
         std::size_t pos = s.find(":");
-        items.insert(std::make_pair(s.substr(0,pos), s.substr(pos+1, s.length())));
+        grammar.insert(std::make_pair(s.substr(0,pos), s.substr(pos+1, s.length())));
     }
 }
-std::string Grammar::get_str_from_char(char c) {
+std::string get_str_from_char(char c) {
     char sym_c[2] = {c, 0};
     return std::string(sym_c);
 }
 
-std::pair<std::set<std::string>, Grammar_type> Grammar::get_bad_non_term() {
+std::pair<std::set<std::string>, Rules> get_bad_non_term(Rules &grammar) {
     std::set<std::string> empty_non_term_first;
     std::set<std::string> empty_non_term_second;
-    Grammar_type bad_rules;
+    Rules bad_rules;
     
     do {
         empty_non_term_first = empty_non_term_second;
         
-        for (Grammar_type::iterator iter = items.begin(); iter!= items.end(); iter++) {
+        for (Rules::iterator iter = grammar.begin(); iter!= grammar.end(); iter++) {
             if (std::all_of(iter->second.begin(), iter->second.end(),
             [&](char c)
             {
@@ -81,40 +46,40 @@ std::pair<std::set<std::string>, Grammar_type> Grammar::get_bad_non_term() {
     return std::make_pair(empty_non_term_first, bad_rules);
 }
 
-void Grammar::proccess_bad_rule(const Rule_type &r, const std::set<std::string> &bad_non_term, Grammar_type &new_items) {
+void proccess_bad_rule(Rules &grammar, const Rule &r, const std::set<std::string> &bad_non_term, Rules &new_grammar) {
     size_t pos = 0;
     std::for_each(r.second.begin(), r.second.end(), 
     [&](char c)
     {
         if (bad_non_term.contains(get_str_from_char(c))) {
-            new_items.insert(r);
+            new_grammar.insert(r);
 
             std::string temp(r.second);
             temp.erase(temp.find(c, pos), 1);
             pos++;
             if (temp != "") {
-                new_items.insert(std::make_pair(r.first, temp));
-                proccess_bad_rule(std::make_pair(r.first, temp), bad_non_term, new_items);
+                new_grammar.insert(std::make_pair(r.first, temp));
+                proccess_bad_rule(grammar, std::make_pair(r.first, temp), bad_non_term, new_grammar);
             }
         }
 
     });
 }
 
-void Grammar::break_rules(std::set<std::string> &bad_non_term, Grammar_type bad_rules) {
-    Grammar_type new_items;
+void break_rules(Rules &grammar, std::set<std::string> &bad_non_term, Rules bad_rules) {
+    Rules new_grammar;
 
-    std::for_each(items.begin(), items.end(),
-    [&](const Rule_type &r) 
+    std::for_each(grammar.begin(), grammar.end(),
+    [&](const Rule &r) 
     {
         if (bad_rules.contains(r)) {
             if (r.first == "S") {
-                new_items.insert(std::make_pair("T", "S"));
-                new_items.insert(std::make_pair("T", ""));
+                new_grammar.insert(std::make_pair("T", "S"));
+                new_grammar.insert(std::make_pair("T", ""));
             }
             
             if (r.second != "") {
-                proccess_bad_rule(r, bad_non_term, new_items);
+                proccess_bad_rule(grammar, r, bad_non_term, new_grammar);
             } 
             //else rule like (T->eplsilon), don`t add
                 
@@ -122,41 +87,38 @@ void Grammar::break_rules(std::set<std::string> &bad_non_term, Grammar_type bad_
             //we need check that right side don`t have bad non term
             if (std::any_of(r.second.begin(), r.second.end(),
             [&](char c){
-                if (bad_non_term.contains(get_str_from_char(c))) {
-                    return true;
-                }
-                return false;
+                return bad_non_term.contains(get_str_from_char(c));
             })
             )
             {
-                proccess_bad_rule(r, bad_non_term, new_items);
+                proccess_bad_rule(grammar, r, bad_non_term, new_grammar);
             } else {
-                new_items.insert(r);
+                new_grammar.insert(r);
             }
         }
     }
     );
     
-    items = new_items;
+    grammar = new_grammar;
 }
 
-void Grammar::delete_simple_rules() {
-    Grammar_type new_items;
+void delete_simple_rules(Rules &grammar) {
+    Rules new_grammar;
 
-    std::for_each(items.begin(), items.end(),
-    [&](const Rule_type &r) 
+    std::for_each(grammar.begin(), grammar.end(),
+    [&](const Rule &r) 
     {
         if (r.first != r.second) {
-            new_items.insert(r);
+            new_grammar.insert(r);
         }
     }
     );
     
-    items = new_items;
+    grammar = new_grammar;
 }
 
-void Grammar::barren() {
-    Grammar_type new_items;
+void barren(Rules &grammar) {
+    Rules new_grammar;
 
     std::set<std::string> non_term_first;
     std::set<std::string> non_term_second;
@@ -164,7 +126,7 @@ void Grammar::barren() {
     do {
         non_term_first = non_term_second;
         
-        for (Grammar_type::iterator iter = items.begin(); iter!= items.end(); iter++) {
+        for (Rules::iterator iter = grammar.begin(); iter!= grammar.end(); iter++) {
             if (std::all_of(iter->second.begin(), iter->second.end(),
             [&](char c)
             {
@@ -177,8 +139,8 @@ void Grammar::barren() {
         }
     } while (non_term_first != non_term_second);
 
-    std::for_each(items.begin(), items.end(),
-    [&](const Rule_type &r) 
+    std::for_each(grammar.begin(), grammar.end(),
+    [&](const Rule &r) 
     {
         if (non_term_first.find(r.first) != non_term_first.end()) {
             if (std::all_of(r.second.begin(), r.second.end(),
@@ -188,22 +150,22 @@ void Grammar::barren() {
                 return (non_term_first.find(get_str_from_char(c)) != non_term_first.end()) || is_term;
             }))
             {
-                new_items.insert(r);
+                new_grammar.insert(r);
             }
         }
     }
     );
 
-    items = new_items;
+    grammar = new_grammar;
 }
 
-void Grammar::unattainable() {
-    Grammar_type new_items;
+void unattainable(Rules &grammar) {
+    Rules new_grammar;
 
     std::set<std::string> non_term_first;
     std::set<std::string> non_term_second;
     
-    if (items.find({"T", ""}) != items.end()) {
+    if (grammar.find({"T", ""}) != grammar.end()) {
         non_term_second.insert("T");
     } else {
         non_term_second.insert("S");
@@ -213,7 +175,7 @@ void Grammar::unattainable() {
 
     do {
         non_term_first = non_term_second;
-        for (Grammar_type::iterator iter = items.begin(); iter!= items.end(); iter++) {
+        for (Rules::iterator iter = grammar.begin(); iter!= grammar.end(); iter++) {
             if (non_term_first.find(iter->first) != non_term_first.end()) {
                 std::for_each(iter->second.begin(), iter->second.end(),
                 [&](char c) 
@@ -228,8 +190,8 @@ void Grammar::unattainable() {
         }
     } while (non_term_first != non_term_second);
 
-    std::for_each(items.begin(), items.end(),
-    [&](const Rule_type &r) 
+    std::for_each(grammar.begin(), grammar.end(),
+    [&](const Rule &r) 
     {
         if (non_term_first.find(r.first) != non_term_first.end()) {
             if (std::all_of(r.second.begin(), r.second.end(),
@@ -238,42 +200,42 @@ void Grammar::unattainable() {
                 return (non_term_first.find(get_str_from_char(c)) != non_term_first.end());
             }))
             {
-                new_items.insert(r);
+                new_grammar.insert(r);
             }
             if (r.second.empty()) {
                 if (empty_too) {
-                    new_items.insert(r);
+                    new_grammar.insert(r);
                 }
             }
         }
     }
     );
 
-    items = new_items;
+    grammar = new_grammar;
 }
 
-void Grammar::cast() {
+void normalize(Rules &grammar) {
     //Step 1
-    std::pair<std::set<std::string>, Grammar_type> temp = get_bad_non_term();
+    std::pair<std::set<std::string>, Rules> temp = get_bad_non_term(grammar);
     std::set<std::string> bad_non_term = temp.first;
-    Grammar_type bad_rules = temp.second;
+    Rules bad_rules = temp.second;
 
     //Step 2, 3
-    break_rules(bad_non_term, bad_rules);
+    break_rules(grammar, bad_non_term, bad_rules);
 
     //Remove barren (бесплодные) syms
-    barren();
+    barren(grammar);
     
     //Remove unattainable(недостижимые) syms
-    unattainable();
+    unattainable(grammar);
 
     //Remove rules like X->X
-    delete_simple_rules();
+    delete_simple_rules(grammar);
 }
 
-void Grammar::print() const {
-    std::for_each(items.begin(), items.end(),
-    [](const Rule_type &r) 
+void print(Rules &grammar) {
+    std::for_each(grammar.begin(), grammar.end(),
+    [](const Rule &r) 
     {
         std::cout << r.first << ":" << r.second << std::endl;
     }
@@ -283,7 +245,9 @@ void Grammar::print() const {
 } 
 
 int main() {
-    solution::Grammar g(std::cin);
-    g.cast();
-    g.print();
+    using namespace solution;
+    Rules grammar;
+    grammar_create(grammar, std::cin);
+    normalize(grammar);
+    print(grammar);
 }
