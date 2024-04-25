a0e6da7b8d75b681e0620e10ba75a16e645d4365
Vladislav <328777dv@bk.ru>
2024-04-25 20:04:33 +0300

    K4 on working (that a disaster)

--- Spring/Ks/K4.cpp
+++ Spring/Ks/K4.cpp
@@ -3,246 +3,201 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <stack>
 
-namespace solution
-{
+typedef std::set<char> State;
+typedef std::map<State, std::map<char, State>> Table;
 
-typedef std::pair<char, std::string> Rule_type;
-typedef std::set<Rule_type> Grammar_type;
-typedef std::set<Rule_type> (Grammar_function)();
-typedef int State_Type;
+typedef std::pair<char, std::string> Rule;
+typedef std::set<Rule> Grammar;
+typedef Grammar (GrammarFunc)(); 
 
-class DFA
-{
-public:
-    DFA(Grammar_function g);
-    DFA(const DFA &) = delete;
-    DFA& operator=(const DFA &) = delete;
-    ~DFA() = default;
+constexpr char BEGIN_STATE = '@';
 
-    bool is_belong(const std::string &);
-
-
-    void print_grammar() {std::for_each(grammar.begin(),grammar.end(), [](const Rule_type &r){std::cout << r.first << " -> " << r.second << std::endl;});}
-    void print_all();
-private:
-    Grammar_type grammar;
-
-    struct Edge
+Table get_table(GrammarFunc get_grammar) {
+    Grammar g = get_grammar();
+    State start = {BEGIN_STATE};
+    Table t;
+    std::for_each(g.begin(), g.end(), 
+    [&](const Rule &r)
     {
-        Edge(int, char, int);
-        
-        bool operator<(const Edge &) const;
-        
-        int from;
+        char from;
+        char to;
         char name;
-        int to;
-    };
 
-    std::set<State_Type> states;
-    std::set<char> alphabet;
-    std::set<Edge> edges;
-    std::set<State_Type> begin_states;
-    std::set<State_Type> end_states;
+        size_t len_right = r.second.length();
+        if (len_right == 2) {
+            // T->Ka
+            from = r.second[0];
+            name = r.second[1];
+            to = r.first;
 
-    static constexpr char BEGIN_STATE_CHAR = '@';
-    static constexpr State_Type BEGIN_STATE = 0;
-    static constexpr State_Type ERROR_STATE = -1;
-
-    int cur_begin_state = BEGIN_STATE;
-
-    void determinate();
-};
-
-DFA::Edge::Edge(int _from, char _name, int _to) :
-    from(_from),
-    name(_name),
-    to(_to)
-{
-}
-
-bool DFA::Edge::operator<(const Edge &other) const {
-    if (from < other.from) return true;
-    if (from > other.from) return false;
-
-    if (name < other.name) return true;
-    if (name > other.name) return false;
-
-    return to < other.to;
-}
-
-
-
-DFA::DFA(Grammar_function g) :
-    grammar(g())
-{
-    // A->Ba ~ B -a- A
-    // A->a
-    // A->
-    states.insert(BEGIN_STATE);
-    begin_states.insert(BEGIN_STATE);
-
-    int new_state = BEGIN_STATE;
-    std::map<char, int> naming_table;
-    naming_table.insert(std::make_pair(BEGIN_STATE_CHAR, new_state++));
-
-    std::for_each(grammar.begin(), grammar.end(),
-    [&](const Rule_type &r)
-    {
-        if (naming_table.find(r.first) == naming_table.end()) {
-            naming_table.insert(std::make_pair(r.first, new_state++));
-        }
-
-        states.insert(naming_table[r.first]);
-
-        std::size_t len = r.second.length();
-        
-        if (len == 2) {
-            // A->Ba
-            alphabet.insert(r.second[1]);
-
-            if (naming_table.find(r.second[0]) == naming_table.end()) {
-                naming_table.insert(std::make_pair(r.second[0], new_state++));
-            }
-
-            states.insert(naming_table[r.second[0]]);
-
-            edges.insert(Edge(naming_table[r.second[0]], r.second[1], naming_table[r.first]));
-
-        } else if (len == 1){
-            // A->a
-            alphabet.insert(r.second[0]);
-            edges.insert(Edge(this->BEGIN_STATE, r.second[0], naming_table[r.first]));
-        } else {
-            // A->epsilon 
-            // TODO: Can it be or not??? 
-            begin_states.insert(r.first);
-        }
-
-    });
-    end_states.insert(naming_table['S']);
-    std::for_each(naming_table.begin(), naming_table.end(), [&](auto p){std::cout << p.first << " ~ " << p.second << std::endl;});
-
-    determinate();
-}
-
-void DFA::determinate() {
-
-}
-
-
-bool DFA::is_belong(const std::string &chain) {
-    State_Type cur_state = cur_begin_state;
-    for (std::size_t i = 0; i < chain.length(); i++) {
-        std::cout << "PARSE " << i << " = " << chain[i] << std::endl;
-        if (std::find(alphabet.begin(), alphabet.end(), chain[i]) != alphabet.end()) {
-            bool moved = false;
-
-            for (auto it = edges.begin(); it != edges.end(); it++) {
-                const Edge &edge = *it;
-                std::cout << "parse edge " << edge.from << ' ' << edge.name << ' ' << edge.to << std::endl;
-                if ((edge.from == cur_state) && (edge.name == chain[i])) {
-                    cur_state = edge.to;
-                    std::cout << "moved!!!!!!!!!!!!!\n";
-                    moved = true;
-                    break;
+            bool found = false;
+            for (Table::iterator p_table = t.begin(); p_table != t.end(); p_table++) {
+                if (std::find(p_table->first.begin(), p_table->first.end(), from) != p_table->first.end()) {
+                    found = true;
+                    p_table->second[name].insert(to);
                 }
             }
 
-            if (!moved) {
-                // Can`t moved
-                std::cout << "CAN`T MOVED\n";
-                cur_state = ERROR_STATE;
+            if (!found) {
+                t[State({from})][name] = State({to});
             }
-        } else {
-            // Symbol not in alphabet
-            std::cout << "SYM ISN`T IN ALPH\n";
-            cur_state = ERROR_STATE;
+
         }
+        if (len_right == 1) {
+            // T->a
+            //from = start; but start type of State
+            name = r.second[0];
+            to = r.first;
 
-        if (cur_state == ERROR_STATE) {
-            break;
+            t[start][name].insert(to);
         }
+    });
 
-        std::cout << "\n";
-    }
+    return t;
 
-    if (cur_state == ERROR_STATE) {
-        return false;
-    } 
-    if (std::find(end_states.begin(), end_states.end(), cur_state) == end_states.end()) {
-        return false;
+}
+
+
+std::map<char, State> operator+(const std::map<char, State>& a, const std::map<char, State>& b) {
+    std::map<char, State> c = a;
+
+    for (const auto& pair : b) {
+        char key = pair.first;
+        const auto& b_set = pair.second;
+        auto& c_set = c[key];
+
+        c_set.insert(b_set.begin(), b_set.end());
     }
     
-    return true;
+    return c;
 }
 
+void determine(Table &t) {
+    std::cout << (t.find({'K', 'S'}) == t.end()) << std::endl;
+    Table det_t;
+    State start = {BEGIN_STATE};
+    det_t[start] = t[start];
+    
+    std::stack<State> queue;
+
+    std::for_each(det_t[start].begin(), det_t[start].end(),
+    [&](const auto &edge)
+    {
+        queue.push(edge.second);
+    });
+
+    std::map<char, State> edges_for_state;
+    while (!queue.empty()) {
+        std::cout << "new stack element" << std::endl;
+        
+        edges_for_state.clear();
+        State cur_state = queue.top();
+        queue.pop();
+
+        std::cout << "parse (";
+        std::for_each(cur_state.begin(), cur_state.end(), [](char c) {std::cout << c;});
+        std::cout << ")" << std::endl;
+
+        std::for_each(cur_state.begin(), cur_state.end(), [&](char nonterm)
+        {
+            std::cout << "merge from " << nonterm << std::endl;
+            std::cout << "t[State({nonterm})]  size = " <<  t[State({nonterm})].size() << std::endl;
+
+            std::for_each(t[State({nonterm})].begin(), t[State({nonterm})].end(),
+            [&](const auto &edge)
+            {
+                std::cout << "edge to merge equ " << std::endl;
+                std::cout << edge.first << ' ' << (edge.first == 0) << std::endl;
+                std::for_each(edge.second.begin(), edge.second.end(), [](char c) {std::cout << c;});
+                std::cout << std::endl;
+            });
+
+            edges_for_state = edges_for_state  + t[State({nonterm})];
+        });
+
+        det_t[cur_state] = edges_for_state;
+
+        std::for_each(det_t[cur_state].begin(), det_t[cur_state].end(),
+        [&](const auto &edge)
+        {
+            std::cout << "next push" << std::endl;
+            std::for_each(edge.second.begin(), edge.second.end(), [](char c) {std::cout << c;});
+            std::cout << std::endl;
+            if (det_t.find(edge.second) == det_t.end()) {
+                std::cout << "pushed\n";
+                queue.push(edge.second);
+            }
+        });
+        std::cout << "----------" << std::endl;
+    }
+    t.clear();
+    t = det_t;
+} 
+
+void print(Table &t) {
+    std::cout << "========================================" << std::endl;
+    for (Table::iterator p_table = t.begin(); p_table != t.end(); p_table++) {
+        std::cout << "(";
+        std::for_each(p_table->first.begin(), p_table->first.end(), [](char c){std::cout << c;});
+        std::cout << ")" << std::endl; 
+        for (auto p_edge = p_table->second.begin(); p_edge != p_table->second.end(); p_edge++) {
+            
+            std::cout << "       " << p_edge->first;
+            
+            std::cout << "(";
+            for (auto iter = p_edge->second.begin(); iter != p_edge->second.end(); iter++) {
+                std::cout << *iter;
+            }
+            std::cout << ")";
+            std::cout << std::endl;
+
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "========================================" << std::endl;
 }
 
-
-#ifdef _main
-std::set<std::pair<char, std::string>> g() {
+Grammar g() {
     return {
-        std::make_pair('S', std::string("Aa")), 
-        std::make_pair('S', std::string("a")), 
-        std::make_pair('A', std::string("a")), 
+        {'S', "Sa"},
+        {'S', "Ma"},
+        {'S', "Ka"},
+        {'S', "Mb"},
+        {'S', "c"},
+        {'M', "Ka"},
+        {'M', "c"},
+        {'K', "c"},
     };
 }
-#endif
 
 int main() {
-    solution::DFA dfa(g);
-    dfa.print_all();
-
-    std::string s;
-    while (std::getline(std::cin, s)) {
-        if (dfa.is_belong(s) == true) {
-            std::cout << "YES" << std::endl;
-        } else {
-            std::cout << "NO" << std::endl;
-        }
-        std::cout << '\n';
-    }
-    return 0;
+    Table table = get_table(g);
+    print(table);
+    determine(table);
+    print(table);
+    // std::string s;
+    // while (std::getline(std::cin, s)) {
+    //     if (belong(table, s)) {
+    //         std::cout << "YES" << std::endl;
+    //     } else {
+    //         std::cout << "NO" << std::endl;
+    //     }
+    // }
 }
 
+/*
+    Table t;
 
-
-void solution::DFA::print_all() 
-    {
-        std::cout << "===================================" << std::endl;
-        std::cout << "DFA:" << std::endl;
-        std::cout << "STATES: sz = " << states.size() << std::endl;
-        std::cout << "  ";
-        std::for_each(states.begin(), states.end(), [](State_Type c){std::cout << c << " , ";});
-        std::cout << std::endl;
-        std::cout << std::endl;
-
-        std::cout << "ALPHABET: sz = " << alphabet.size() << std::endl;
-        std::cout << "  ";
-        std::for_each(alphabet.begin(), alphabet.end(), [](char c){std::cout << c << " , ";});
-        std::cout << std::endl;
-        std::cout << std::endl;
-
-        std::cout << "EDGES: sz = " << edges.size() << std::endl;
-        std::cout << "  ";
-        std::for_each(edges.begin(), edges.end(), [](const Edge &edge)
-        {
-            std::cout << edge.from << " -" << edge.name << "- " << edge.to << std::endl;
-            std::cout << "  ";
-        });
-        std::cout << std::endl;
-
-        std::cout << "BEGIN_STATES: sz = " << begin_states.size() << std::endl;
-        std::cout << "  ";
-        std::for_each(begin_states.begin(), begin_states.end(), [](State_Type c){std::cout << c << " , ";});
-        std::cout << std::endl;
-        std::cout << std::endl;
-
-        std::cout << "END_STATES: sz = " << end_states.size() << std::endl;
-        std::cout << "  ";
-        std::for_each(end_states.begin(), end_states.end(), [](State_Type c){std::cout << c << " , ";});
-        std::cout << std::endl;
-        std::cout << std::endl;
-        std::cout << "===================================" << std::endl;
+    for sym in s:
+    t[cur_state] //set pair
+    for_each t[cur_state]{
+        pair p;
+        if (p.first == cur_sym) {
+            cur_state = p.second;
+            break;
+        }
     }
+
+*/
