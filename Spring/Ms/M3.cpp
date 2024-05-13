642f62e4ea5d5a65e2f87dc0278884e2948ff760
Vladislav <328777dv@bk.ru>
2024-05-13 15:57:14 +0300

    M3 on working... (with prints)

---
+++ Spring/Ms/M3.cpp
@@ -0,0 +1,266 @@
+#include <iostream>
+#include <exception>
+#include <string>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <algorithm>
+
+/*
+    Грамматика:
+
+    Для лучшего понимания нетерминалы обозначены не одной заглавной буквой, 
+    а Словом (между словами для читаемости допускается пробел)
+    
+    
+
+    Чистая без действий: 
+
+    S -> Assign Comma
+    Comma -> ',' S | eps
+    Assign -> Arithm Equal
+    Equal -> '=' Assign | eps
+    Arithm -> Op Sub
+    Sub -> '-' Op Sub | eps
+    Op -> Var | Num | '(' S ')'
+    Var -> буква
+    Num -> цифра 
+
+
+    С действиями:
+
+    <init helpful vars>
+    S -> Assign Comma
+    Comma -> ',' <lvalue = false> S | eps
+    Assign -> Arithm Equal
+    Equal -> '=' <if (!lvalue) ERROR> Assign <assign var> | eps
+    Arithm -> Op Sub
+    Sub -> '-' <> Arithm <lvalue = false, calculate sub> | eps
+    Op -> Var | Num | '(' S ')'
+    Var -> буква <lvalue = true, stack.push(буква)>
+    Num -> цифра <lvalue = false, stack.push(цифра)>
+
+*/
+
+class Parser
+{
+public:
+    Parser() = default;
+    void parse(std::istream &);
+
+    int get_value() const;
+    void print_vars() {std::for_each(vars.begin(), vars.end(), [](auto p) {std::cout << static_cast<char>(p.first) << ':' << p.second << std::endl;});}
+private:
+    int c = 0;
+    std::istream *in = nullptr;
+
+    void gc();
+
+    void S();
+    void Assign();
+    void Comma();
+    void Arithm();
+    void Equal();
+    void Op();
+    void Sub();
+    void Var();
+    void Num();
+
+    bool lvalue = false;
+    int value = 0;
+    std::map<int, int> vars;
+    std::stack<int> stack;
+};
+
+void Parser::gc() {
+    c = in->get();
+}
+
+void Parser::parse(std::istream &_in) {
+    /* < */
+    lvalue = false;
+    vars.clear();
+    stack = std::stack<int>();
+    /* > */
+
+    in = &_in;
+    gc();
+    S();
+
+    if (c != EOF) {
+        std::cout << "EOF on " << static_cast<char> (c) << " ";
+        throw c;
+    }
+
+    value = stack.top();
+    stack.pop();
+
+}
+
+void Parser::S() {
+    Assign(); 
+    Comma();
+}
+
+void Parser::Comma() {
+    if (c == ',') {
+        /* < */
+        lvalue = false;
+        stack.pop();
+        /* > */
+
+        gc();
+        S();
+    }
+}
+
+void Parser::Assign() {
+    Arithm();
+    Equal();
+}
+
+void Parser::Equal() {
+    if (c == '=') {
+        /* < */
+        if (!lvalue) {
+            std::cout << "not lvalue ";
+            throw c;
+        }
+        /* > */
+
+        gc();
+        Assign();
+    
+        /* < */
+        int op = stack.top();
+        stack.pop();
+        int var = stack.top();
+        stack.pop();
+
+        vars[var] = op;
+        stack.push(op);
+        /* > */
+
+    }
+}
+
+void Parser::Arithm() {
+    Op();
+    Sub();
+}
+
+void Parser::Sub() {
+    if (c == '-') {
+        /* < */
+
+        /* > */
+        
+        gc();
+        Op();
+
+        /* < */
+        lvalue = false;
+        int op2 = stack.top();
+        stack.pop();
+        int op1 = stack.top();
+        stack.pop();
+        //check that operand is var
+        if (op1 > 9) {
+            if (vars.find(op1) == vars.end()) {
+                std::cout << "uninit var ";
+                throw c;
+            } else {
+                op1 = vars[op1];
+            }
+        }
+        if (op2 > 9) {
+            if (vars.find(op2) == vars.end()) {
+                std::cout << "uninit var ";
+                throw c;
+            } else {
+                op2 = vars[op2];
+            }
+        }
+        std::cout << "sub " << static_cast<char>(op1+'0') << '-' << static_cast<char>(op2+'0') << std::endl; 
+        stack.push(op1 - op2);
+        /* > */
+
+        Sub();
+    }
+}
+
+void Parser::Op() {
+    if (std::isalpha(c)) {
+        Var();
+    } else if (std::isdigit(c)) {
+        Num();
+    } else if (c == '(') {
+        gc();
+        S();
+        if (c != ')') {
+            std::cout << ") ";
+            throw c;
+        } else {
+            gc();
+        }
+    } else {
+        std::cout << "Op ";
+        throw c;
+    }
+
+}
+
+void Parser::Var() {
+    if (!std::isalpha(c)) {
+        std::cout << "Var ";
+        throw c;
+    } else {
+
+        /* < */
+        lvalue = true;
+        stack.push(c);
+        /* > */
+
+        gc();
+    }
+}
+
+void Parser::Num() {
+    if (!std::isdigit(c)) {
+        std::cout << "Num ";
+        throw c;
+    } else {
+
+        /* < */
+        lvalue = false;
+        stack.push(c - '0');
+        /* > */
+
+        gc();
+    }
+}
+
+int Parser::get_value() const { return value; }
+
+int main() {
+    Parser parser;
+    
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std::stringstream input(line);
+        std::cout << "____________\n";
+        std::cout << line << std::endl;
+        try
+        {
+            parser.parse(input);
+            std::cout << "YES, value is " << parser.get_value() << std::endl;
+            parser.print_vars();
+        }
+        catch(int c)
+        {
+            std::cout << std::endl << "NO" << std::endl;
+        }
+        std::cout << "____________\n";
+    }
+    return 0;
+}
